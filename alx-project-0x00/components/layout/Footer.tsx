39a9539c660a95f8bc60786749db39232e7115a9
lenny288 <lennymbuthi1@gmail.com>
2025-09-21 06:11:06 -0700

    Update Footer.tsx

--- alx-project-0x00/components/layout/Footer.tsx
+++ alx-project-0x00/components/layout/Footer.tsx
@@ -1,1 +1,12 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-full bg-gray-900 text-white text-center py-6 mt-12">
+      <p>© {new Date().getFullYear()} StayFinder. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
 
