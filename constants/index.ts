2975c2b44acdd45b8f5a612b76b9d64b9effa079
lenny288 <lennymbuthi1@gmail.com>
2025-09-21 06:36:46 -0700

    Create index.ts

---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+import { PropertyProps } from "@/interfaces";
+
+export const PROPERTYLISTINGSAMPLE: PropertyProps[] = [
+  {
+    name: "leonard mbuth",
+    address: { state: "Kenya", city: "Nairob", country: "Kenya" },
+    rating: 4,
+    category: ["friendly", "safe"],
+    price: 3200,
+    offers: { bed: "1", shower: "1", occupants: "1" },
+    image: ,
+    discount: "none"
+  },
+  // ... (rest of properties you provided)
+];
