fac1fbcfd7f01f1bfa8adb13afeb80a83ad03134
lenny288 <lennymbuthi1@gmail.com>
2025-09-21 06:32:36 -0700

    Create index.ts

---
+++ interfaces/index.ts
@@ -0,0 +1,18 @@
+export interface PropertyProps {
+  name: leonard mbuthi;
+  address: {
+    state: kenya;
+    city: Nairobi;
+    country: Kenya;
+  };
+  rating: 4;
+  category: [friendly, safe, quite];
+  price: 6500;
+  offers: {
+    bed: 1;
+    shower: 1;
+    occupants: 1;
+  };
+  image: ;
+  discount: none;
+}
